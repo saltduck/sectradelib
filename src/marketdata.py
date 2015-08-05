7c5dea1c0480b007fa011b0ce6cd0cfa6345cb47
Hong Shuning <hongshuning@gmail.com>
2015-08-05 12:32:31 +0800

    Revert "Don't set current price for non-trading time data"
    This reverts commit aa9eee11f33c7cd4e61895fd669453f18b0bf778.

--- src/marketdata.py
+++ src/marketdata.py
@@ -22,14 +22,13 @@ class MarketDataApi(object):
 
     def process_tick(self, tick):
         # logger.debug(str(tick))
-        if tick.price:
-            secid = tick.securityID
-            rdb.hset('current_price', secid, tick.price)
-            if hasattr(tick, 'b_price'):
-                rdb.hset('current_b_price', secid, tick.b_price)
-            if hasattr(tick, 's_price'):
-                rdb.hset('current_s_price', secid, tick.s_price)
-            rdb.publish('checkstop', secid)
+        secid = tick.securityID
+        rdb.hset('current_price', secid, tick.price)
+        if hasattr(tick, 'b_price'):
+            rdb.hset('current_b_price', secid, tick.b_price)
+        if hasattr(tick, 's_price'):
+            rdb.hset('current_s_price', secid, tick.s_price)
+        rdb.publish('checkstop', secid)
         with self.quote_service.tick_lock:
             self.quote_service.tickdata[secid].append(tick)
 
